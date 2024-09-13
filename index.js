adea6f978653f0ad1f963723569a045eb7a8be45
pradeep9451 <pradeepyadav945141@gmail.com>
2024-09-13 15:39:22 +0530

    apihost

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+
+const apiData = require("./data.json");
+
+app.get("/", (req, res) =>{
+res.send("Hello I am live");
+ });
+
+ app.get("/service", (req,res)=>{
+   res.send(apiData);
+ });
+
+app.listen(port, () => {
+     console.log("I am live again"); 
+    });
